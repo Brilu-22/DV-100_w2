fcbefd8736535daff3bf354ffede26a7bb6f212c
brilu-22 <231214@virtualwindow.co.za>
2023-07-30 22:00:04 +0200

    second commit
    my links still don't work

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,31 @@ $(document).ready(function(){
 
     console.log("Hello")
 
+    // --------------------------------------------------------------------------------
+    // Home page 
+
     // when the image loads 
     $(".hero-image").animate({top: '-=100px'})
+
+     // --------------------------------------------------------------------------------
+    // Browse Page 
+
+    $("#description-text").hide();
+
 });
+
+// when the card is clicked 
+
+$(".card").click(function(){
+
+// Toggle price and description text
+
+$("#price-text").toggle();
+$("description-text").toggle();
+
+// Resize the image to fit additional content 
+$(".card-image-top").toggleClass("small")
+
+})
+
+   
